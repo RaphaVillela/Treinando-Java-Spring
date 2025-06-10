8c109eee8d08cbb0a24ffa3864b629f01c16bdda
RaphaVillela <raphinha22villela@gmail.com>
2025-06-09 21:46:40 -0300

    Lambdas e Streams

--- src/main/java/br/com/site/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/site/screenmatch/ScreenmatchApplication.java
@@ -1,30 +1,31 @@
 package br.com.site.screenmatch;
 
+import br.com.site.screenmatch.main.Main;
+import br.com.site.screenmatch.model.DadosEpisodio;
 import br.com.site.screenmatch.model.DadosSerie;
+import br.com.site.screenmatch.model.DadosTemporada;
 import br.com.site.screenmatch.service.ConsumoApi;
 import br.com.site.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		//String busca = game of thrones&Season=1;
-		//String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=902024ae";
-		//var url = "https://api.adviceslip.com/advice";
-		var url = "https://www.omdbapi.com/?t=game+of+thrones&Season=1&apikey=902024ae";
-		var json = consumoApi.obterDados(url);
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+
+		Main main = new Main();
+		main.exibeMenu();
+
 	}
 }
--- src/main/java/br/com/site/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/site/screenmatch/model/DadosTemporada.java
@@ -1,5 +1,11 @@
 package br.com.site.screenmatch.model;
 
-public record DadosTemporada(Integer numero,
-                             List<DadosEpisodio>) {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(@JsonAlias("Season") Integer numero,
+                             @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
 }
